08587e463301ed2f82c91630a3a4abeacde8d5d8
csarpy <cody.sarpy@gmail.com>
2025-07-08 15:39:22 -0400

    Added hit box, player shape

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
-import constants as c
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    print(f"Screen width: {c.SCREEN_WIDTH}")
-    print(f"Screen height: {c.SCREEN_HEIGHT}")
-
-    screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
